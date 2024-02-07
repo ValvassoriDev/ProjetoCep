bdb0b96b7c4cf7264869c7f192b2e5f500e045d2
Mateus Valvassori <valvassori.dev@gmail.com>
2024-02-07 14:51:42 -0300

    retorno ao original

--- ProjetoCep/Produtos/CepResult.cs
+++
@@ -1,7 +0,0 @@
-﻿// CepResult.cs
-public class CepResult
-{
-    public bool IsSuccess { get; set; }
-    public string UppercaseJsonResponse { get; set; }
-    public string ErrorMessage { get; set; }
-}
--- ProjetoCep/Produtos/IcepService.cs
+++
@@ -1,6 +0,0 @@
-﻿// ICepService.cs
-public interface ICepService
-{
-    bool IsValidCep(string cep, out string errorMessage);
-    Task<CepResult> GetCepInformation(string cep);
-}
